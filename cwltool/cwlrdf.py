c33d3ea121862ec493b2dd1f06227c775dd9ce94
ThomasHickman <Thomas.Hickman42@gmail.com>
2018-05-08 15:53:10 +0100

    Final changes

--- cwltool/cwlrdf.py
+++ cwltool/cwlrdf.py
@@ -15,6 +15,9 @@ from .process import Process
 import sys
 import textwrap
 
+from collections import Counter
+
+import re
 
 def gather(tool, ctx):  # type: (Process, ContextType) -> Graph
     g = Graph()
@@ -101,6 +104,12 @@ def get_end_url(url):
 def get_url_hash(url):
     return str(url).split("#")[-1]
 
+def get_before_hash(uri):
+    if uri.rfind("#") == -1:
+        return uri
+
+    return str(uri)[:uri.rfind("#")]
+
 def get_end_name(name):
     hash_regions = str(name).split("#")
     if len(hash_regions) == 2:
@@ -120,6 +129,9 @@ def print_indent(string):
     print(" " * indent_level + string)
 
 def get_tool_name(url):
+    if url.rfind("/") < url.rfind("#"):
+        return url
+
     return "/".join(url.split("/")[:-1])
 
 tool_names = set()
@@ -145,12 +157,12 @@ from pprint import pprint
 import json
 
 drawn_workflows = set()
-default_num = 0
+uuid_num = 0
 
-def get_udefault():
-    global default_num
-    default_num += 1
-    return f"default{default_num}"
+def get_uid():
+    global uuid_num
+    uuid_num += 1
+    return f"{uuid_num}"
 
 def esc(string):
     if isinstance(string, bool):
@@ -167,26 +179,86 @@ def esc(string):
         .replace("{", "\\{") \
         .replace("}", "\\}")
 
-def get_workflow_dot(tool):
+arrows = []
+transforms = []
+
+def tu(uri):
+    global transforms
+    for transform in transforms:
+        uri = uri.replace(*transform)
+
+    return uri
+
+ids_by_workflow = dict()
+# def get_(input_param):
+#     for re.findall(r"\$\((.*?)\)", input_param["valueFrom"])
+
+def get_props_str(props_dict):
+    if props_dict == {}:
+        return ""
+
+    props_arr = []
+    for key, value in props_dict.items():
+        props_arr.append(f"{key}=\"{value}\"")
+
+    return "[" + ", ".join(props_arr) + "]"
+
+def get_workflow_dot(tool, repeat_times, workflow_id):
     global drawn_workflows
     global tool_names
     global indent_level
-    print_indent(f"""subgraph "cluster_{get_end_name(tool.tool["id"])}" {{""")
+    global arrows
+    global transforms
+    global ids_by_workflow
+    drawing_workflow_id = workflow_id
+    ids_by_workflow[tool.tool["id"]] = workflow_id
+    def draw_node(node_id, label, **props):
+        props["label"] = label
+
+        if props.get("peripheries") is None:
+            props["peripheries"] = repeat_times
+
+        print_indent(f""""{tu(node_id)}{"" if node_id [0:4] != "file" else ("#" + workflow_id)}" {get_props_str(props)};""")
+
+
+    def draw_arrow(source, target, label=None, is_double_arrow=False, source_step=None, **props):
+        if label is not None:
+            props["label"] = f"  {label}  "
+
+        if is_double_arrow:
+            props["arrowhead"] = "normalnormal"
+
+        print(f"Output step name “{source_step}”", file=sys.stderr)
+        source_num = ids_by_workflow.get(source_step, ids_by_workflow.get(get_before_hash(source)))
+        target_num = ids_by_workflow.get(get_before_hash(target))
+        try:
+            if source[0:4] == "file":
+                assert source_num is not None
+            if target[0:4] == "file":
+                assert target_num is not None
+        except:
+            import pdb; pdb.set_trace()
+
+        arrows.append(f""""{source}{"" if source_num is None else "#" + str(source_num)}" -> "{target}{"" if target_num is None else "#" + str(target_num)}" {get_props_str(props)};""")
+
+    print_indent(f"""subgraph "cluster_{get_end_name(tool.tool["id"])}{get_uid()}" {{""")
     indent_level += 2
+    print_indent(f"""color=grey""")
     print_indent(f"""label="{get_end_name(tool.tool["id"])}\";""")
 
-    print_indent("subgraph cluster_inputs {")
+    print_indent(f"subgraph cluster_inputs{get_uid()} {{")
     indent_level += 2
     print_indent("rank = \"same\";")
     print_indent("style = \"dashed\";")
     print_indent("label = \"Workflow Inputs\";")
+    input_steps = set()
     for cwl_input in tool.tool["inputs"]:
-        print_indent(f""""{cwl_input["id"]}" [label="{get_end_name(cwl_input["id"])}", fillcolor="#94DDF4"];""")
+        input_steps.add(cwl_input["id"])
+        draw_node(cwl_input["id"], get_end_name(cwl_input["id"]), fillcolor="#94DDF4")
         # TODO add a label
     indent_level -= 2
     print_indent("}")
 
-    arrows = []
     inputs_id_to_end_id = dict()
     outputs_id_to_end_id = dict()
     workflows_to_draw = []
@@ -200,26 +272,41 @@ def get_workflow_dot(tool):
         for y in cwl_step.tool["outputs"]:
             outputs_id_to_end_id[y["id"]] = y["endId"]
 
+    steps_by_id = dict()
     for cwl_step in tool.steps:
-        if cwl_step.embedded_tool.tool["id"] in drawn_workflows:
-            pass
+        steps_by_id[cwl_step.id] = cwl_step
+
+    for dict_cwl_step in tool.tool["steps"]:
+        cwl_step = steps_by_id[dict_cwl_step["id"]]
+        drawing_workflow_id = workflow_id
+        step_target_suffix = ""
         if isinstance(cwl_step.embedded_tool, Workflow):
+            drawing_workflow_id = get_uid()
+
+            ids_by_workflow[cwl_step.id] = drawing_workflow_id
+            print(f"Adding “{cwl_step.id}”", file=sys.stderr)
             drawn_workflows.add(cwl_step.embedded_tool.tool["id"])
-            workflows_to_draw.append(cwl_step.embedded_tool)
-            # get_workflow_dot(cwl_step.embedded_tool)
+            #workflows_to_draw.append(cwl_step.embedded_tool)
+            inner_wf_repeat_times = repeat_times
+            if cwl_step.tool.get("scatter") is not None:
+                inner_wf_repeat_times += 1
+            get_workflow_dot(cwl_step.embedded_tool, inner_wf_repeat_times, drawing_workflow_id)
         else:
-            props = []
-            props.append(f"""label="{get_end_name(cwl_step.id)}\"""")
+            props = {}
 
             if isinstance(cwl_step.embedded_tool, ExpressionTool):
-                props.append(f"""fillcolor="#d3d3d3\"""")
+                props["fillcolor"] = "#d3d3d3"
             else:
                 assert isinstance(cwl_step.embedded_tool, CommandLineTool)
 
-            if cwl_step.tool.get("scatter") is not None:
-                props.append(f"""peripheries=2""")
+            item_repeat_times = repeat_times
 
-            print_indent(f""""{cwl_step.id}" [{", ".join(props)}];""")
+            if cwl_step.tool.get("scatter") is not None:
+                item_repeat_times += 1
+
+            props["peripheries"] = item_repeat_times
+
+            draw_node(cwl_step.id, get_end_name(cwl_step.id), **props)
 
         for cwl_step_input in cwl_step.tool["inputs"]:
             arrow_target = cwl_step_input["endId"]
@@ -228,14 +315,19 @@ def get_workflow_dot(tool):
             else:
                 pass
 
-            if cwl_step_input.get("default") is not None \
-                    or (cwl_step_input.get("valueFrom") is not None \
-                    and cwl_step_input.get("source") is None):
-                value = cwl_step_input.get("default", cwl_step_input.get("valueFrom"))
-                default_node_name = get_udefault()
-                print_indent(f""""{default_node_name}" [label="{esc(value)}", fillcolor="#d5aefc"]""")
+            if cwl_step_input.get("source") is None and cwl_step_input.get("valueFrom") is not None:
+                # old_regex = r"\$\(inputs\.(\w+).*?\)"
+                js_expressions = re.findall(r"inputs\.(\w+).*?", cwl_step_input["valueFrom"])
+                if len(js_expressions) != 0:
+                    cwl_step_input["source"] = list(map(lambda x: tool.tool["id"] + "#" + x, js_expressions))
 
-                arrows.append(f""""{default_node_name}" -> "{arrow_target}" [label="  {get_out_name(cwl_step_input["id"])}  "];""")
+            if cwl_step_input.get("source") is None:
+                assert cwl_step_input.get("valueFrom") is not None or cwl_step_input.get("default") is not None
+                value = cwl_step_input.get("default", cwl_step_input.get("valueFrom"))
+                default_node_name = f"fixed_name{get_uid()}"
+                draw_node(default_node_name, esc(value), fillcolor="#d5aefc")
+
+                draw_arrow(default_node_name, arrow_target, get_out_name(cwl_step_input["id"]))
             else:
                 assert cwl_step_input.get("source") is not None
                 if isinstance(cwl_step_input["source"], str):
@@ -244,11 +336,15 @@ def get_workflow_dot(tool):
                     source_list = cwl_step_input["source"]
 
                 if cwl_step_input.get("valueFrom") is not None:
-                    value_from_node_name = get_udefault()
-                    print_indent(f""""{value_from_node_name}" [label="{esc(cwl_step_input["valueFrom"])}", fillcolor="#d5aefc"]""")
+                    value_from_node_name = f"value_from_node{get_uid()}"
+                    draw_node(value_from_node_name, esc(cwl_step_input["valueFrom"]), fillcolor="#ffa07a")
 
                 for source_item in source_list:
                     assert isinstance(source_item, str)
+
+                    is_double_arrow = False
+                    if cwl_step.tool.get("scatter") is not None and cwl_step_input["id"] in cwl_step.tool["scatter"]:
+                        is_double_arrow = True
 
                     if get_tool_name(source_item) in tool_names:
                         arrow_source = get_tool_name(source_item)
@@ -256,19 +352,23 @@ def get_workflow_dot(tool):
                         arrow_source = outputs_id_to_end_id.get(source_item, source_item)
 
                     if cwl_step_input.get("valueFrom") is None:
-                        arrows.append(f""""{arrow_source}" -> "{arrow_target}" [label="  {get_out_name(source_item)}  "];""")
+                        if arrow_source in input_steps:
+                            # i.e. this is an input step
+                            draw_arrow(arrow_source, arrow_target, source_step=get_tool_name(source_item), is_double_arrow=is_double_arrow)
+                        else:
+                            draw_arrow(arrow_source, arrow_target, get_out_name(source_item), is_double_arrow, source_step=get_tool_name(source_item))
                     else:
-                        arrows.append(f""""{arrow_source}" -> "{value_from_node_name}\"""")
-                        arrows.append(f""""{value_from_node_name}" -> "{arrow_target}" [label="  {get_out_name(source_item)}  "];""")
+                        draw_arrow(arrow_source, value_from_node_name, source_step=get_tool_name(source_item))
+                        draw_arrow(value_from_node_name, arrow_target, get_out_name(source_item))
 
-    print_indent("subgraph cluster_outputs {")
+    print_indent(f"subgraph cluster_outputs{get_uid()} {{")
     indent_level += 2
     print_indent("rank = \"same\";")
     print_indent("style = \"dashed\";")
     print_indent("labelloc = \"b\";")
     print_indent("label = \"Workflow Outputs\";")
     for cwl_output in tool.tool["outputs"]:
-        print_indent(f""""{cwl_output["id"]}" [label="{get_end_name(cwl_output["id"])}", fillcolor="#94DDF4"];""")
+        draw_node(cwl_output["id"], get_end_name(cwl_output["id"]), fillcolor="#94DDF4")
 
         if isinstance(cwl_output["outputSource"], str):
             cwl_output_source_list = [cwl_output["outputSource"]]
@@ -283,18 +383,14 @@ def get_workflow_dot(tool):
             else:
                 arrow_source = outputs_id_to_end_id[cwl_output_source]
 
-            arrows.append(f""""{arrow_source}" -> "{cwl_output["id"]}" [label="  {get_out_name(cwl_output_source)}  ", fillcolor="#94DDF4"];""")
-    indent_level -= 2
-    print_indent("}")
-
-    for arrow in arrows:
-        print_indent(arrow)
+            draw_arrow(arrow_source, cwl_output["id"], get_out_name(cwl_output_source), source_step=get_tool_name(cwl_output_source))
 
     indent_level -= 2
     print_indent("}")
 
-    for workflow in workflows_to_draw:
-        get_workflow_dot(workflow)
+    indent_level -= 2
+    print_indent("}")
+
 
 start = """
     digraph workflow {
@@ -303,6 +399,7 @@ start = """
         color = "black"
         fontsize = "10"
         clusterrank = "local"
+        newrank = true # NOTE: is this attribute is not set, the graph doesn't display very well at all
         # labeljust = "left"
         # ranksep = "0.22"
         # nodesep = "0.05"
@@ -330,9 +427,12 @@ start = """
 
 def cwl_viewer_dot(tool_json):
     global indent_level
+    global arrows
     print(textwrap.dedent(start))
     indent_level = 2
-    get_workflow_dot(tool_json)
+    get_workflow_dot(tool_json, 1, get_uid())
+    for arrow in arrows:
+        print_indent(arrow)
     print("}")
 
 
